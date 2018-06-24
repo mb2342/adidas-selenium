8c2e724d4e67d6579e1c36b31519217532997234
Maxim <mdogx.g@gmail.com>
2018-06-25 01:06:50 +0400

    Added steps

---
+++ src/test/java/samplePageTest.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import tk.mdogx.adidas.selenium.Utils;
+import tk.mdogx.adidas.selenium.pages.HomePage;
+
+import java.util.concurrent.TimeUnit;
+
+public class samplePageTest {
+
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        driver = Utils.getDriver();
+        driver.manage().timeouts().implicitlyWait(Utils.TIMEOUT, TimeUnit.SECONDS);
+        driver.navigate().to(Utils.BASEURL);
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void testDown() {
+//        driver.quit();
+    }
+
+    @Test
+    public void addComments() {
+
+        final String errorPageMessageWrongEmail = "ERROR: please enter a valid email address.";
+        final String errorPageTitleWrongEmail = "Comment Submission Failure";
+
+        new HomePage(driver)
+                .openSamplePage()
+                .sendNewCommentWithWrongEmail()
+                .checkErrorMessage(errorPageTitleWrongEmail, errorPageMessageWrongEmail)
+                .sendNewComment()
+                .checkPostedComment();
+    }
+}
